232df09ea97933b82c8772a13bac36c7bb7425ae
Julianjuyo <je.oliverosf@uniandes.edu.co>
2024-05-04 01:32:04 -0500

    test 2

--- tests/test_puppeteer/test_2/test_2.js
+++ tests/test_puppeteer/test_2/test_2.js
@@ -31,46 +31,39 @@ const pathfolder = path.resolve(__dirname);
       "#admin-x-settings-scroller > div > div:nth-child(3) > div > div:nth-child(3) > section > div.block.undefined > div > div > button"
     );
 
-    // Crear membresía premium
-    await page.click(
-      "#admin-x-settings-scroller > div > div:nth-child(3) > div > div:nth-child(3) > section > div.block.undefined > div > div > button"
+    // Buscar la sección de descuentos
+    await page.type(
+      'input[type="text"][placeholder="Search settings"]',
+      "offers",
+      { delay: 100 }
     );
-    await page.waitForSelector("#modal-backdrop > section > div.p-7.py-0");
 
-    // Llenar formulario
-    await page.type('input[type="text"][placeholder="Bronze"]', "test", {
-      delay: 100,
-    });
-    await page.type(
-      'input[type="text"][placeholder="Full access to premium content"]',
-      "Membrería Premium ",
-      { delay: 100 }
-    );
-    await page.type('input[type="text"][placeholder="5"]', "5", { delay: 100 });
-    await page.type('input[type="text"][placeholder="50"]', "48", {
-      delay: 100,
-    });
-    await page.type(
-      'input[type="text"][placeholder="Expert analysis"]',
-      "Obtén acceso a todo el contenido web",
-      { delay: 100 }
-    );
+    // POR EL MOMENTO LE HAGO CLICK YO
+    // await page.click("button.cursor-pointer");
+
+    await new Promise((r) => setTimeout(r, 2000));
+
+    // Crear descuento
     await page.click("button.cursor-pointer.bg-green");
-    await new Promise((r) => setTimeout(r, 1000));
-    await page.screenshot({
-      path: `${pathfolder}/img/2_formulario-membresia.png`,
-    });
-    await page.click(
-      "#modal-backdrop > section > div.w-100.sticky.bottom-\\[-24px\\].z-\\[297\\].m-0.box-border.p-0 > div.sticky.z-\\[299\\].mb-\\[-24px\\].flex.items-center.justify-between.h-\\[96px\\].bg-white.dark\\:bg-black > div > div.flex.gap-3 > div > button.cursor-pointer.bg-black.text-white.dark\\:bg-white.dark\\:text-black.hover\\:bg-grey-900.inline-flex.items-center.justify-center.whitespace-nowrap.rounded.text-sm.transition.font-bold.h-\\[34px\\].px-4.min-w-\\[80px\\]"
-    );
-    await new Promise((r) => setTimeout(r, 1000));
-    await page.waitForSelector(
-      'button[data-testid="exit-settings"]'
-    );
-    await page.screenshot({
-      path: `${pathfolder}/img/3_after-formulario-membresia.png`,
-    });
 
+    // llenar formulario oferta
+    await page.waitForSelector('input[type="text"][placeholder="Black Friday"]');
+    await page.type(
+      'input[type="text"][placeholder="Black Friday"]',
+      "c1",
+      { delay: 100 }
+    );
+    await page.type('input[type="number"]', "5");
+    await page.screenshot({ path: `${pathfolder}/img/2_se_llena_formulario.png` });
+
+    await new Promise((r) => setTimeout(r, 2000));
+    
+    await page.click("button.cursor-pointer.bg-black");
+    await new Promise((r) => setTimeout(r, 2000));
+
+
+    await page.screenshot({ path: `${pathfolder}/img/3_oferta_creada.png` });
+    await page.click('button.cursor-pointer.p-2');
 
     //Cerrar sesión desde ajustes
     await page.click('button[data-testid="exit-settings"]');
@@ -78,13 +71,12 @@ const pathfolder = path.resolve(__dirname);
       "body > div.gh-app > div > nav.gh-nav > div > header"
     );
 
-
     //Cerrar sesión desde menu principal
     await page.click(".ember-basic-dropdown-trigger");
     await page.waitForSelector(".user-menu-signout");
     await page.click(".user-menu-signout");
-
     await page.waitForSelector("#identification");
+
     await page.screenshot({ path: `${pathfolder}/img/4_finaliza.png` });
 
     console.log("Test finalizado con éxito");
